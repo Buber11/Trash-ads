cd2d06d338f73cbf18adc1bfd827fc097f823c6d
Jakub <263998@student.pwr.edu.pl>
2025-03-27 18:04:39 +0100

    feat: add database init.sql file

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,65 @@
+
+CREATE TYPE listing_status AS ENUM ('active', 'inactive', 'sold');
+CREATE TYPE reservation_status AS ENUM ('pending', 'confirmed', 'canceled');
+
+
+CREATE TABLE address (
+    id SERIAL PRIMARY KEY,
+    state VARCHAR(100) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    street VARCHAR(100) NOT NULL,
+    house_number VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    address_id BIGINT REFERENCES address(id) ON DELETE SET NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE user_details (
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    phone VARCHAR(11) UNIQUE
+);
+
+CREATE TABLE listings (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    price NUMERIC(10,2) NOT NULL,
+    photo VARCHAR(255),
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    status_lis listing_status DEFAULT 'active',
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE reservations (
+    id SERIAL PRIMARY KEY,
+    listing_id INT REFERENCES listings(id) ON DELETE CASCADE,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    status_res reservation_status DEFAULT 'pending',
+    order_price NUMERIC(10,2) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE category_listing (
+    listing_id INT REFERENCES listings(id) ON DELETE CASCADE,
+    category_id INT REFERENCES categories(id) ON DELETE CASCADE,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    PRIMARY KEY (listing_id, category_id)
+);
